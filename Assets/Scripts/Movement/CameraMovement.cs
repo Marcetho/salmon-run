1e832d1dea190bf5f9435c6d37866b5cc553aaf2
choopedpotat <greentintin@gmail.com>
2025-01-04 17:49:34 -0800

    more stuff

---
+++ Assets/Scripts/Movement/CameraMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float smoothness = 5.0f;
+    public Transform targetObject;
+    private Vector3 initalOffset;
+    private Vector3 cameraPosition;
+
+    void Start()
+    {  
+        initalOffset = transform.position - targetObject.position;
+    }
+
+    void Update()
+    {
+        cameraPosition = targetObject.position + initalOffset;
+        transform.position = cameraPosition;
+
+        transform.rotation = targetObject.rotation;
+    }
+}
